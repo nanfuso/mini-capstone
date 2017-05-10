bff07a460003874aed26117f28a1198eaa4fb6dc
natail <nataleanfuso@gmail.com>
2017-05-09 21:28:57 -0500

    made it look pretty

--- app/models/compter.rb
+++ app/models/compter.rb
@@ -1,2 +1,17 @@
 class Compter < ApplicationRecord
+    def sale_alert
+        sale_message = ""
+        sale_message = "Discount item!" if price.to_i < 20 
+        sale_message = "Everyday Value!" if price.to_i >= 20
+        sale_message
+    end
+
+    def tax
+        tax = price.to_i * 0.09
+        tax.round(3)
+    end
+
+    def total_price
+        price.to_i + tax
+    end
 end
